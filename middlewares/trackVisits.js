61ec35dff7e429159085aa02aff1bdcbaa5f128d
doctamayot <doctamayot@hotmail.com>
2019-04-08 21:09:09 -0500

    done

--- middlewares/trackVisits.js
+++ middlewares/trackVisits.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const pageView = require("../models/pageView");
 
 const trackVisit = async (req, res, next) => {
-  const pageVisit = await pageView.create({ path: req.route.path, userAgent: req.get('User-Agent') })
+  const pageVisit = await pageView.create({ path: req.path, userAgent: req.get('User-Agent') })
   console.log(pageVisit)
   next();
 }
